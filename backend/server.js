99d52fd63ab94b72628be835181a47613f8b3c7a
Gururaj <gururaj@netskope.com>
2023-07-27 17:42:04 +0530

    MERN App REST API changes added

---
+++ backend/server.js
@@ -0,0 +1,29 @@
+require('dotenv').config()
+const express = require('express')
+const mongoose = require('mongoose')
+const workoutRoutes = require('./routes/workouts')
+
+// Create app
+const app = express()
+
+// Middleware
+app.use(express.json())
+app.use((req, res, next) => {
+    console.log(req.path, req.method)
+    next();
+})
+
+// app route
+app.use('/api/workouts', workoutRoutes)
+
+// Connnect to the mongodb
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => {
+    // Listen for the request
+    app.listen(process.env.PORT, () => {
+        console.log('connected to mongobd and listening at port ', process.env.PORT)
+    });
+  })
+  .catch((error) => {
+    console.log(error)
+  })
