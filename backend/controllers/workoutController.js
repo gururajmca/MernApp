61af8bfba17da9efe0001eafb917cc620d8589c0
Gururaj <gururaj@netskope.com>
2023-08-06 16:28:30 +0530

    Completed the MERN application with all the basic functionalities

--- backend/controllers/workoutController.js
+++ backend/controllers/workoutController.js
@@ -4,6 +4,19 @@ const mongoose = require('mongoose')
 // Create new workout
 const createNewWorkout = async (req, res) => {
     const {title, load, reps} = req.body
+    let emptyFields = []
+    if (!title) {
+      emptyFields.push('title')
+    }
+    if (!load) {
+      emptyFields.push('load')
+    }
+    if (!reps) {
+      emptyFields.push('reps')
+    }
+    if (emptyFields.length > 0) {
+      return res.status(400).json({ error: 'Please fill in all fields', emptyFields })
+    }
     try {
       const workout = await Workout.create({title, load, reps})
       res.status(200).json(workout)
